26fc4e4262714e83b0911e52af208a33d393ab5f
Garrett Vance <garrettsvance@gmail.com>
2024-10-05 14:55:21 -0600

    created loginpresenter

---
+++ tweeter-web/src/presenter/LoginPresenter.ts
@@ -0,0 +1,25 @@
+import {
+  AuthenticatePresenter,
+  AuthenticationView,
+} from "./AuthenticatePresenter";
+
+export class LoginPresenter extends AuthenticatePresenter {
+  public constructor(view: AuthenticationView) {
+    super(view);
+  }
+
+  public async doLogin(alias: string, password: string) {
+    this.setLoadingState(true);
+
+    try {
+      const [user, authToken] = await this.userService.login(alias, password);
+      this.authenticated(user, authToken);
+    } catch (error) {
+      this.displayErrorMessage(
+        `Failed to login user because of exception: ${error}`,
+      );
+    } finally {
+      this.setLoadingState(false);
+    }
+  }
+}
