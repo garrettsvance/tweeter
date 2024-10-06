3a28f372beae60998c661fdf81471d6859242bc5
garrettpants <garrettsvance@gmail.com>
2024-10-06 17:56:46 -0600

    removed references of fakedata

--- tweeter-web/src/components/userInfo/userNavigationHook.ts
+++ tweeter-web/src/components/userInfo/userNavigationHook.ts
@@ -1,4 +1,3 @@
-import { AuthToken, FakeData, User } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "./userInfoHook";
 import React from "react";
