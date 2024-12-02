a1546e85bdf245ec2cc8db34519811fd35ef3a7c
Garrett Vance <garrettsvance@gmail.com>
2024-12-02 13:54:11 -0700

    created some functions and made notes in followsdynamo

--- tweeter-server/src/model/dao/dynamo/FollowsDAODynamo.ts
+++ tweeter-server/src/model/dao/dynamo/FollowsDAODynamo.ts
@@ -16,6 +16,9 @@ export class FollowsDAODynamo implements FollowsDAO {
   private readonly followerName: string = "followerName";
   private readonly followerAlias: string = "followerAlias";
   private readonly followerImage: string = "followerImage";
+  private readonly followeeName: string = "followeeName";
+  private readonly followeeAlias: string = "followeeAlias";
+  private readonly followeeImage: string = "followeeImage";
   private readonly client = DynamoDBDocumentClient.from(new DynamoDBClient());
 
   public async followAction(follower: string, followee: string): Promise<void> {
@@ -34,12 +37,41 @@ export class FollowsDAODynamo implements FollowsDAO {
     }
   }
 
-  getFollowees(
+  public async getFollowees(
     user: User | null,
     pageSize: number,
     userAlias: string,
   ): Promise<[followers: UserDto[], hasMore: boolean]> {
-    return Promise.resolve([[], false]);
+    const params = {
+      TableName: this.tableName,
+      IndexName: this.followerIndex,
+      KeyConditionExpression: "#followee = :alias",
+      ExpressionAttributeNames: {
+        "#followee": this.followeeAlias,
+      },
+      ExpressionAttributeValues: {
+        ":alias": userAlias,
+      },
+      Limit: pageSize,
+    };
+    try {
+      const response = await this.client.send(new QueryCommand(params));
+      const followees = response.Items
+        ? response.Items.map((item) => {
+            const [firstName, lastName] = item[this.followeeName].split(", ");
+            return new User(
+              firstName,
+              lastName,
+              item[this.followeeAlias],
+              item[this.followeeImage],
+            ).dto;
+          })
+        : [];
+      const hasMore = !!response.LastEvaluatedKey;
+      return [followees, hasMore];
+    } catch {
+      throw new Error("Error retrieving followers");
+    }
   }
 
   public async getFollowers(
@@ -79,12 +111,30 @@ export class FollowsDAODynamo implements FollowsDAO {
     }
   }
 
-  getIsFollower(alias: string, aliasToFollow: string): Promise<boolean> {
-    return Promise.resolve(false);
+  //TODO: double check the differences with index and table names, and use that when searching for your information
+  // for followers and followees
+
+  public async getIsFollower(
+    alias: string,
+    aliasToFollow: string,
+  ): Promise<boolean> {
+    const params = {
+      TableName: this.tableName,
+      Key: {
+        followerAlias: alias,
+        followeeAlias: aliasToFollow,
+      },
+    };
+    try {
+      const response = await this.client.send(new GetCommand(params));
+      return response.Item != null;
+    } catch {
+      throw new Error("Error getting isFollower status");
+    }
   }
 
   getNumFollowee(alias: string): Promise<number> {
-    return Promise.resolve(0);
+    return Promise.resolve(0); // select: select.COUNT
   }
 
   getNumFollower(alias: string): Promise<number> {
